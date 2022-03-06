f94be15a0f58ba1cc13bfa40aca63113034ad5be
irinaamsn <ira.myasnikova.02@list.ru>
2022-03-06 19:16:11 +0300

    release v1

---
+++ сортировки.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+#define N 8
+
+int arr[N];
+
+void Rand()
+{
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = rand() % 201 - 100;
+    }
+    cout << "BEFORE: " << endl;
+    for (int i = 0; i < N; i++)
+    {
+        cout << arr[i] << "\t"; 
+    }
+    cout << endl;
+}
+
+void Bubbles()
+{
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+        }
+    
+}
+
+void Inserts()
+{
+    for (int i = 1; i < N; i++)
+        for (int j = i-1; (j>=0) && (arr[j]>arr[j+1]); j--)
+             swap(arr[j], arr[j + 1]);
+}
+
+void Output()
+{
+    cout << "AFTER: " << endl;
+    for (int i = 0; i < N; i++)
+        cout << arr[i] << "\t";
+ }
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Rand();
+
+   // Bubbles();
+     Inserts();
+
+     Output();
+}
+
